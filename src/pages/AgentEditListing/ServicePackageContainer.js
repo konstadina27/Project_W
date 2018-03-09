8f5041ead18c64a18e12c7173e772ba6d020f28a
Konstadina Laiou <konstadina27@gmail.com>
2018-03-09 22:34:35 +0200

    ServicePackageContainer updated

---
+++ src/pages/AgentEditListing/ServicePackageContainer.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { EditorState, convertToRaw} from 'draft-js';
+import {ServicesPackageContent} from './ServicePackage'
+
+export class ServicesPackage extends React.Component {
+	constructor(props){
+		super(props);
+		this.state={
+			service: [
+			{id:0, editorState:EditorState.createEmpty(),servicesTitle:"",servicesVideo:"",servicesPrice:"",serviceFiles:""}
+			]
+		}
+	}
+	onEditorStateChange(editorState){
+		this.setState({
+	      editorState,
+	    });
+  	};
+  	handleServiceValueChange(event){
+		this.setState({
+			[event.target.name]: event.target.value
+		})
+	}
+    handleServiceSubmit(event) {
+		let service = this.state.service
+		service.push({
+			id: (service.length + 1) - 1,
+			servicesTitle: this.state.servicesTitle,
+			servicesVideo: this.state.servicesVideo,
+			servicesPrice: this.state.servicesPrice,
+			serviceFiles:this.state.serviceFiles,
+			editorState:JSON.stringify(convertToRaw(this.state.editorState.getCurrentContent()))
+		})
+		this.setState({
+			service: service
+		})
+        console.log(service);
+        event.preventDefault();
+      }
+	render(){
+		return(
+			<ServicesPackageContent handleServiceSubmit={this.handleServiceSubmit.bind(this)} editorState={this.state.editorState}
+			 handleServiceValueChange={this.handleServiceValueChange.bind(this)} onEditorStateChange={this.onEditorStateChange.bind(this)}/>
+		)
+	}
+}
